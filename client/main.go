d9bb39e1e5c54c62b3c3031b7811ec1e97066cbd
Rishav Raj <rr679122@gmail.com>
2024-09-08 22:44:12 +0530

    first commit

---
+++ client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/rishavABCD/grpc/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.NewClient(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NamesList{
+		Names: []string{"Rishav", "Alice", "Bob"},
+	}
+
+	callSayHello(client)
+	callSayHelloServerStream(client, names)
+	callSayHelloClientStream(client, names)
+	callSayHelloBidirectionalStream(client, names)
+}
